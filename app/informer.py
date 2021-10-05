ae3494cb6b1184170f0acfa4ef478726f38d53fc
Дмитрий А. Воробьев <ditryvorobev@yoomoney.ru>
2021-10-05 20:41:00 +0300

    revert

--- app/informer.py
+++ app/informer.py
@@ -850,12 +850,12 @@ async def send_message_to_users(request):
         for acc in data_json['accounts']:
             user_from_db = await db().get_users('account_name', re.sub('@yamoney.ru|@yoomoney.ru|@', '', acc))
             if len(user_from_db) > 0:
-                if user_from_db[0]['tg_login'] != None and user_from_db[0]['working_status'] != 'dismissed':
-                    set_of_chat_id.append(user_from_db[0]['tg_login'])
+                if user_from_db[0]['tg_id'] != None and user_from_db[0]['working_status'] != 'dismissed':
+                    set_of_chat_id.append(user_from_db[0]['tg_id'])
 
         logger.info('sending message for %s', set_of_chat_id)
         for chat_id in set_of_chat_id:
-            await send_message_to_tg_chat('@'+chat_id, data_json['text'], disable_notification, ParseMode.HTML, escape_html)
+            await send_message_to_tg_chat(chat_id, data_json['text'], disable_notification, ParseMode.HTML, escape_html)
 
     if 'jira_tasks' in data_json:
         for task in data_json['jira_tasks']:
