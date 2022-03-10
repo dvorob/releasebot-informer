733eb66d2afa7053b48a8c4eb87d86de8ef2e220
your_username <your_email@gmail.com>
2022-03-10 11:55:13 +0300

    add fix for dismissed user into filter
    fix
    
    fix

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -66,24 +66,25 @@ async def restricted(message: types.message) -> bool:
         Проверка, что пользователь есть в БД бота (== он заведён в AD и активен). Если нет - это не наш юзер, отвечать ему не нужно.
     """
     try:
-        user_info = await db().search_users_by_account(str(message.from_user.username))
-        logger.info('restriction check for : %s ,\n from %s ,\n found in users table %s', message.from_user.username, sys._getframe().f_back.f_code.co_name, user_info)
+        users_db = await db().search_users_by_account(str(message.from_user.username))
+        logger.info(f"restriction check for : {message.from_user.username} ,\n from {sys._getframe().f_back.f_code.co_name} ,\n found in users table {users_db}")
         warning_message = f'Unauthorized access denied: {returnHelper.return_name(message)}'
 
             # Две проверки: 1 - что у нас в принципе есть ответ, если нет, напишем пользователю, что мы его не наши. 
             #               2 - что у найденного пользователя есть account_name в AD
-        if len(user_info) > 0:
-            logger.info('restricted allow for %s', user_info[0]['account_name'])
-            if str(message.from_user.id) != user_info[0]['tg_id']:
-                await db().set_users(user_info[0]['account_name'], full_name=None, tg_login=None, working_status=None, tg_id=str(message.from_user.id), notification=None, email=None)
-            if user_info[0]['working_status'] != 'dismissed':
+        if len(users_db) > 0:
+            users = sorted(users_db, key=lambda d: d['working_status'], reverse=True)
+            logger.info(f"restricted allow for {users[0]['account_name']}")
+            if str(message.from_user.id) != users[0]['tg_id']:
+                await db().set_users(users[0]['account_name'], full_name=None, tg_login=None, working_status=None, tg_id=str(message.from_user.id), notification=None, email=None)
+            if users[0]['working_status'] != 'dismissed':
                 return True
             else:
                 await message.answer(text='Извините, но вы уволились. Не пишите мне больше, у нас теперь разная жизнь.')
                 return False
         else:
-            logger.info('restricted user not found %s %s ', message.from_user.username, warning_message)
+            logger.info(f"restricted user not found {message.from_user.username} {warning_message}")
             await message.answer(text='Ваш telegram-логин не найден в базе пользователей компании. Обратитесь к администраторам.')
             return False
-    except Exception:
-        logger.exception('exception in restricted')
+    except Exception as e:
+        logger.exception(f"Exception in restricted {str(e)}")
